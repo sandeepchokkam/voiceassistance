615a131c27e5cfd5fae4fecd128c1284f1acc64e
chokkam sandeep <chokkamsandeep9822@gmail.com>
2022-05-11 22:27:33 +0530

    second version

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import wikipedia
 import random
 import os
 from datetime import date
-#sample comments
+#sample comment
   
 # this method is for taking the commands
 # and recognizing the command from the
